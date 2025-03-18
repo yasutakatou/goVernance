00169df6c052717d8de680d22aac51801712c174
yasutakatou <katoh.yasuta@gmail.com>
2025-03-18 09:33:27 +0900

    2

--- governance.go
+++ governance.go
@@ -1,17 +1,106 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
+	"regexp"
+	"time"
+)
+
+var (
+	debug, logging bool
 )
 
 func main() {
-	url := "http://google.co.jp"
+	_Debug := flag.Bool("debug", false, "[-debug=debug mode (true is enable)]")
+	_Logging := flag.Bool("log", false, "[-log=logging mode (true is enable)]")
+
+	flag.Parse()
+
+	debug = bool(*_Debug)
+	logging = bool(*_Logging)
+
+	os.Exit(0)
+}
+
+func debugLog(message string) {
+	var file *os.File
+	var err error
+
+	if debug == true {
+		fmt.Println(message)
+	}
+
+	if logging == false {
+		return
+	}
+
+	const layout = "2006-01-02_15"
+	const layout2 = "2006/01/02 15:04:05"
+	t := time.Now()
+	filename := t.Format(layout) + ".log"
+	logHead := "[" + t.Format(layout2) + "] "
+
+	if Exists(filename) == true {
+		file, err = os.OpenFile(filename, os.O_WRONLY|os.O_APPEND, 0666)
+	} else {
+		file, err = os.OpenFile(filename, os.O_WRONLY|os.O_CREATE, 0666)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer file.Close()
+	fmt.Fprintln(file, logHead+message)
+}
+
+func Exists(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
+func urlget(url string) string {
 
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 
 	byteArray, _ := ioutil.ReadAll(resp.Body)
-	fmt.Println(string(byteArray))
+
+	return string(byteArray)
+}
+
+func loadConfig(configFile string) {
+	loadOptions := ini.LoadOptions{}
+	loadOptions.UnparseableSections = []string{"define", "whitelist", "blacklist"}
+
+	cfg, err := ini.LoadSources(loadOptions, configFile)
+	if err != nil {
+		fmt.Printf("Fail to read config file: %v", err)
+		os.Exit(1)
+	}
+
+	ocr = ""
+	linter = ""
+
+	setStructs("define", cfg.Section("define").Body())
+	setStructs("whitelist", cfg.Section("whitelist").Body())
+	setStructs("blacklist", cfg.Section("blacklist").Body())
+}
+
+func setStructs(configType, datas string) []string {
+	var strs []string
+	debugLog(" -- " + configType + " --")
+
+	for _, v := range regexp.MustCompile("\r\n|\n\r|\n|\r").Split(datas, -1) {
+		if len(v) > 0 {
+			strs = append(strs, v)
+			debugLog(v)
+		}
+	}
+	return strs
 }
