ec556cf7a809a70764370e66cb99038c5bf57826
yasutakatou <katoh.yasuta@gmail.com>
2025-03-20 13:58:16 +0900

    20250320 1358

--- governance.go
+++ governance.go
@@ -2,21 +2,29 @@ package main
 
 import (
 	"bufio"
+	"bytes"
+	"context"
 	"flag"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 )
 
 var (
-	debug, logging               bool
-	define, whitelist, blacklist []string
-	defines                      []defineStruct
+	debug, logging                      bool
+	input, output, whitelist, blacklist []string
+	defines                             []defineStruct
 )
 
 type defineStruct struct {
@@ -24,6 +32,14 @@ type defineStruct struct {
 	Command []string
 	Limit   int
 	Alert   string
+}
+
+// FYI: https://journal.lampetty.net/entry/capturing-stdout-in-golang
+type Capturer struct {
+	saved         *os.File
+	bufferChannel chan string
+	out           *os.File
+	in            *os.File
 }
 
 func main() {
@@ -40,12 +56,66 @@ func main() {
 	loadConfig(*_Config)
 	defineGet(*_Define)
 	loadDefine(*_Define)
+	runCommand()
 
 	os.Exit(0)
 }
 
+func runCommand() {
+	for i := 0; i < len(defines); i++ {
+		//if checkWhitelist(defines[i].Command) == true && checkBacklist(defines[i].Command) == true {
+		//	result := cmdExec(defines[i].Command)
+		//}
+	}
+}
+
+func cmdExec(command string) string {
+	var cmd *exec.Cmd
+	var out string
+
+	debugLog("command: " + command)
+
+	cmd = exec.Command(os.Getenv("SHELL"), "-c", command)
+
+	c := &Capturer{}
+	c.StartCapturingStdout()
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	cmd.Stdin = os.Stdin
+	cmd.Run()
+
+	out = c.StopCapturingStdout()
+	debugLog(out)
+	return out
+}
+
+// 標準出力をキャプチャする
+func (c *Capturer) StartCapturingStdout() {
+	c.saved = os.Stdout
+	var err error
+	c.in, c.out, err = os.Pipe()
+	if err != nil {
+		panic(err)
+	}
+
+	os.Stdout = c.out
+	c.bufferChannel = make(chan string)
+	go func() {
+		var b bytes.Buffer
+		io.Copy(&b, c.in)
+		c.bufferChannel <- b.String()
+	}()
+}
+
+// キャプチャを停止する
+func (c *Capturer) StopCapturingStdout() string {
+	c.out.Close()
+	os.Stdout = c.saved
+	return <-c.bufferChannel
+}
+
 func defineGet(filename string) {
-	if len(define) == 0 {
+	if len(input) == 0 {
 		return
 	}
 
@@ -57,11 +127,11 @@ func defineGet(filename string) {
 	}
 	defer file.Close()
 
-	for i := 0; i < len(define); i++ {
-		splitStr := strings.Split(define[i], "\t")
+	for i := 0; i < len(input); i++ {
+		splitStr := strings.Split(input[i], "\t")
 		switch splitStr[0] {
 		case "s3":
-			//s3 file get
+			fmt.Fprint(file, s3get(splitStr[1], splitStr[2]))
 		case "url":
 			fmt.Fprint(file, urlget(splitStr[1]))
 		}
@@ -106,12 +176,38 @@ func Exists(filename string) bool {
 }
 
 func urlget(url string) string {
-
 	resp, _ := http.Get(url)
 	defer resp.Body.Close()
 	byteArray, _ := io.ReadAll(resp.Body)
 	debugLog(string(byteArray))
 	return string(byteArray)
+}
+
+func s3get(bucket, key string) string {
+	// AWS設定の読み込み
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+
+	client := s3.NewFromConfig(cfg)
+
+	output, err := client.GetObject(context.TODO(), &s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		log.Fatalf("failed to get object, %v", err)
+	}
+	defer output.Body.Close()
+
+	body, err := ioutil.ReadAll(output.Body)
+	if err != nil {
+		log.Fatalf("failed to read object body, %v", err)
+	}
+
+	debugLog(string(body))
+	return string(body)
 }
 
 func configRead(filename, sectionName string) []string {
@@ -129,7 +225,7 @@ func configRead(filename, sectionName string) []string {
 			if rFlag == true && str[0] == 91 {
 				break
 			} else {
-				if rFlag == true {
+				if rFlag == true && str[0] != 35 {
 					debugLog(str)
 					strs = append(strs, str)
 				}
@@ -144,7 +240,8 @@ func configRead(filename, sectionName string) []string {
 }
 
 func loadConfig(filename string) {
-	define = configRead(filename, "define")
+	input = configRead(filename, "input")
+	output = configRead(filename, "output")
 	whitelist = configRead(filename, "whitelist")
 	blacklist = configRead(filename, "blacklist")
 }
